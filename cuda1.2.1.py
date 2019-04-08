3ee2e777230b163eed60d4f627398b09db579c37
Karolina <karolina.pawlaczyk@gmail.com>
2019-04-08 15:12:23 +0200

    added measuring time

--- cuda1.2.1.py
+++ cuda1.2.1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import datetime
 
 # Find_first() function returns a value of pi, when the difference 
 # between consecutive elements in the list is smaller than
@@ -16,6 +17,10 @@ eps = 0.00001
 a = np.arange(n)
 arr = (4 * a ** 2) / (4 * a ** 2 - 1)
 
+start = datetime.datetime.now()
 ff = find_first(eps, arr)
 for f in ff:
     print(f'Found value: {f}')
+end = datetime.datetime.now()
+elapsed_time = end - start
+print(f'Done in: {elapsed_time}')
